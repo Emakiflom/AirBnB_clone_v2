00815012d1e77a104dd89c16f6f956e735ae2023
Aman Kflom <125467587+Emakiflom@users.noreply.github.com>
2024-04-14 18:10:05 +0300

    change split func and remove do_pack func

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,21 +9,8 @@ from datetime import datetime
 
 env.user = 'ubuntu'
 env.hosts = ['54.158.180.95', '100.26.10.90']
+env.key_filenmae = '~/.ssh/school'
 
-
-def do_pack():
-    """
-    Targginng project directory into a packages as .tgz
-    """
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-    local('sudo mkdir -p ./versions')
-    path = './versions/web_static_{}'.format(now)
-    local('sudo tar -czvf {}.tgz web_static'.format(path))
-    name = '{}.tgz'.format(path)
-    if name:
-        return name
-    else:
-        return None
 
 
 def do_deploy(archive_path):
@@ -31,7 +18,7 @@ def do_deploy(archive_path):
     """
     try:
         archive = archive_path.split('/')[-1]
-        path = '/data/web_static/releases/' + archive.strip('.tgz')
+        path = '/data/web_static/releases/' + archive.split('.')[0]
         current = '/data/web_static/current'
         put(archive_path, '/tmp')
         run('mkdir -p {}/'.format(path))
@@ -43,5 +30,6 @@ def do_deploy(archive_path):
         run('ln -s {} {}'.format(path, current))
         print('New version deployed!')
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
