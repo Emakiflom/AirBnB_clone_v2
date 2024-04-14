58cef38418d1ac55fa94e2215361a0deab8b73de
Aman Kflom <125467587+Emakiflom@users.noreply.github.com>
2024-04-14 13:37:33 +0300

    update ngnix to listen on port 80

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,14 +5,9 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" >> /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+echo -e "<html>\n<head>\n</head>\n<body>\n  Holberton School\n</body>\n</html>" > /data/web_static/releases/test/index.html
+sudo rm - rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
