94f2caad540de0c1654c677a15d9d3f676af538c
Aman Kflom <125467587+Emakiflom@users.noreply.github.com>
2024-04-21 20:53:30 +0300

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,8 +1,8 @@
+@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr  21  2024
-
+Created on Tue Sep  1 11:15:54 2020
 @author: Robinson Montes
 """
 from flask import Flask
@@ -23,4 +23,3 @@ def hbnb():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
