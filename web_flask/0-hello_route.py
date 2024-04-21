e3fceac3b421f883804539875e7e74168da6a890
Aman Kflom <125467587+Emakiflom@users.noreply.github.com>
2024-04-21 20:48:34 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,10 +8,12 @@ Created on Sun Apr  21  2024
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """Start a basic Flask web application"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
