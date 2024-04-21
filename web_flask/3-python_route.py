d32c0d41459fd8ea92863f4a172bdedcd2e8c431
Aman Kflom <125467587+Emakiflom@users.noreply.github.com>
2024-04-21 20:57:58 +0300

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,15 +3,17 @@
 """
 Created on Sun Apr  21  2024
 
-@author: Robinson Montes
+@author: Aman Hablu
 """
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     """Start a basic Flask web application"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -24,11 +26,13 @@ def c_text(text=None):
     """Dynamic inputed text: replace _ for space and show text"""
     return "C {}".format(text.replace('_', ' '))
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<string:text>', strict_slashes=False)
 def python_text(text='is_cool'):
     """Dynamic inputed text: replace _ for space and show text"""
     return "Python {}".format(text.replace('_', ' '))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
